299fcc15d5a8280e87534f90bd44bad88da8ec33
Tsuku-Yomi <523896791@qq.com>
2022-01-03 22:55:14 +0800

    编写网络类

---
+++ Network.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ATP_Server {
+    class Network {
+        Socket serverSocket;
+        public Network(int port=7332) {
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, port);
+            serverSocket = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(ip);
+            serverSocket.Listen(10);
+        }
+
+        public void Run() {
+
+        } 
+    }
+}
